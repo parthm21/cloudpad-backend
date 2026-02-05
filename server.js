1f183cf9bb5efdcc9da521f40abb869448cf110a
parthm21 <parthmadhoriya21@gmail.com>
2026-02-05 17:53:24 +0530

    Fix static files for CSS and theme

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
